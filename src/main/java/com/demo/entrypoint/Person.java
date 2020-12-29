5860e5afaa24899a05c0f3fd13dcc02bb3125111
michals96 <michal.stefaniuk9@gmail.com>
2020-12-29 15:54:26 +0100

    List product names. First database connection

--- src/main/java/com/demo/entrypoint/Person.java
+++ src/main/java/com/demo/entrypoint/Person.java
@@ -1,4 +1,9 @@
 package com.demo.entrypoint;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Person {
 	String name;
@@ -12,6 +17,26 @@ public class Person {
 	}
 	
 	public String greetings() {
+		String url = "jdbc:postgresql://localhost:5432/testdb?user=postgres&password=postgres";
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Class not found " + e);
+		}
+		
+		try {
+			Connection conn = DriverManager.getConnection(url);
+			Statement statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM products");
+			
+			while(resultSet.next()){
+				System.out.println(resultSet.getString("name"));
+			}
+			
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
 		return "Hello " + this.getName();
 	}
 
